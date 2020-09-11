d9a600b38a5a3b370f47a0926644de29ca02f5c6
xujiawei <xujiaweimail@163.com>
2020-09-11 11:14:23 +0800

    定义还款api

--- src/test/java/com/cfckata/repay/RepayControllerTest.java
+++ src/test/java/com/cfckata/repay/RepayControllerTest.java
@@ -4,6 +4,14 @@ import com.cfckata.common.ApiTest;
 
 public class RepayControllerTest extends ApiTest{
 
-	//根据还款计划ID扣款
+	//根据还款计划ID扣款-余额不足扣款失败场景
+	public void repay_by_id_amt_lack() {
+		
+	}
+	
+	
+	//根据还款计划ID扣款-成功场景
+	
+	//批量扣款
 	
 }
