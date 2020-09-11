2820a7f64e8e15ee2ae760819cb884c0ad99ea78
wuming <wuming@hnzycfc.com>
2020-09-11 11:17:48 +0800

    添加请求

---
+++ src/main/java/com/cfckata/reqeust/Customer.java
@@ -0,0 +1,52 @@
+/**
+ * @title Customer.java
+ * @author wuming
+ * @date 2020年9月11日 上午11:07:10
+ * @copyright 2020 中原消费金融有限公司版权所有
+ */
+package com.cfckata.reqeust;
+
+/**
+ * @Description <描述>
+ * @author wuming
+ * @date 2020年9月11日 上午11:07:10
+ */
+public class Customer {
+    private String id;
+    private String name;
+    private String idnumber;
+    private String mobilePhone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdnumber() {
+        return idnumber;
+    }
+
+    public void setIdnumber(String idnumber) {
+        this.idnumber = idnumber;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+}
