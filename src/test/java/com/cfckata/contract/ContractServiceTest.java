5e2c6960ed072c77ad00276c31d118398bc64f74
wuming <wuming@hnzycfc.com>
2020-09-11 15:29:20 +0800

    合同

---
+++ src/test/java/com/cfckata/contract/ContractServiceTest.java
@@ -0,0 +1,58 @@
+/**
+ * @title ContractServiceTest.java
+ * @author wuming
+ * @date 2020年9月11日 下午3:20:09
+ * @copyright 2020 中原消费金融有限公司版权所有
+ */
+package com.cfckata.contract;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cfckata.common.SpringServiceTest;
+import com.cfckata.contract.common.CustomerDto;
+import com.cfckata.contract.domain.Contract;
+import com.cfckata.contract.reqeust.CreateContractRequest;
+import com.cfckata.contract.service.ContractService;
+
+/**
+ * @Description <描述>
+ * @author wuming
+ * @date 2020年9月11日 下午3:20:09
+ */
+public class ContractServiceTest extends SpringServiceTest{
+
+    private ContractService service;
+    
+    @Before
+    public void setUp() throws Exception {
+        service = new ContractService();
+    }
+    
+    @Test
+    public void should_pay_success_when_checkout_a_normal_order() {
+        //Given
+     //   doNothing().when(payProxy).pay(anyString(), any());
+
+        String id = "BJ001";
+        String name = "张三";
+        String idnumber =  "411327198609043116";
+        String mobilePhone="18537118170";
+        CustomerDto customer = new CustomerDto(id, name, idnumber, mobilePhone);
+        
+        BigDecimal interestRate = new BigDecimal("9.9");
+        String repaymentType = "DEBX";
+        String maturityDate = "2020-09-10";
+        BigDecimal commitment = new BigDecimal("9000");
+
+       CreateContractRequest request = new CreateContractRequest(customer, interestRate, repaymentType, maturityDate, commitment);
+    
+       Contract contract = service.createContract(request);
+
+       assertThat(contract.getContractId()).isNotNull();
+    }
+}
